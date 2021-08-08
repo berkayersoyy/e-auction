b7a4fe9b6a0704eee19a6be45e3004b4f533108d
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-08 14:47:13 +0300

    Sourcing entities created.

---
+++ EAuction.Sourcing.Api/Entities/Auction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EAuction.Sourcing.Api.Entities
+{
+    public class Auction
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime FinishedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Status { get; set; }
+        public List<string> IncludeSellers { get; set; }
+    }
+
+    public enum Status
+    {
+        Active = 0,
+        Closed = 1,
+        Passive = 2
+    }
+}
---
+++ EAuction.Sourcing.Api/Entities/Bid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EAuction.Sourcing.Api.Entities
+{
+    public class Bid
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string AuctionId { get; set; }
+        public string ProductId { get; set; }
+        public string SellerUserName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
