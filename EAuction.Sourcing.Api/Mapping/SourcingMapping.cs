efa9edf1ba70adfe79c6d88c58d998e7b2309037
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-08 14:49:50 +0300

    Mapping with AutoMapper added.

---
+++ EAuction.Sourcing.Api/Mapping/SourcingMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EAuction.Sourcing.Api.Entities;
+using EventBusRabbitMQ.Events;
+
+namespace EAuction.Sourcing.Api.Mapping
+{
+    public class SourcingMapping : Profile
+    {
+        public SourcingMapping()
+        {
+            CreateMap<OrderCreateEvent, Bid>().ReverseMap();
+        }
+    }
+}
