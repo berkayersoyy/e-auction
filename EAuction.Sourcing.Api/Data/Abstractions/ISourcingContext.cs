c770e8668064e67ba46dfe035412643159821600
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-08 14:47:32 +0300

    MongoDb contexts added.

---
+++ EAuction.Sourcing.Api/Data/Abstractions/ISourcingContext.cs
@@ -0,0 +1,12 @@
+﻿using EAuction.Sourcing.Api.Entities;
+using MongoDB.Driver;
+
+namespace EAuction.Sourcing.Api.Data.Abstractions
+{
+    public interface ISourcingContext
+    {
+        IMongoCollection<Auction> Auctions { get; }
+        IMongoCollection<Bid> Bids { get; }
+
+    }
+}
---
+++ EAuction.Sourcing.Api/Data/SourcingContext.cs
@@ -0,0 +1,23 @@
+﻿using EAuction.Sourcing.Api.Data.Abstractions;
+using EAuction.Sourcing.Api.Entities;
+using EAuction.Sourcing.Api.Settings;
+using MongoDB.Driver;
+
+namespace EAuction.Sourcing.Api.Data
+{
+    public class SourcingContext:ISourcingContext
+    {
+        public SourcingContext(ISourcingDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            Auctions = database.GetCollection<Auction>(nameof(Auction));
+            Bids = database.GetCollection<Bid>(nameof(Bid));
+
+            SourcingContextSeed.SeedData(Auctions);
+        }
+        public IMongoCollection<Auction> Auctions { get; }
+        public IMongoCollection<Bid> Bids { get; }
+    }
+}
