963b4094fdbaad257ea82fc492bb84fa96f67037
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:10:48 +0300

    Order entity added.

---
+++ EAuction.Order.Domain/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EAuction.Order.Domain.Entities.Base;
+
+namespace EAuction.Order.Domain.Entities
+{
+    public class Order:Entity
+    {
+        public string AuctionId { get; set; }
+        public string SellerUserName { get; set; }
+        public string ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
