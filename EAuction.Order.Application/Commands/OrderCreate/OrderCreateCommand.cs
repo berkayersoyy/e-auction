2020a8d6c542eb19fdffcfb62a9e2787f2901da1
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:13:23 +0300

    Order commands added.

---
+++ EAuction.Order.Application/Commands/OrderCreate/OrderCreateCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EAuction.Order.Application.Responses;
+using MediatR;
+
+namespace EAuction.Order.Application.Commands.OrderCreate
+{
+    public class OrderCreateCommand: IRequest<OrderResponse>
+    {
+        public string AuctionId { get; set; }
+        public string SellerUserName { get; set; }
+        public string ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ EAuction.Order.Application/Commands/OrderCreate/OrderCreateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace EAuction.Order.Application.Commands.OrderCreate
+{
+    public class OrderCreateValidator:AbstractValidator<OrderCreateCommand>
+    {
+        public OrderCreateValidator()
+        {
+            RuleFor(x => x.SellerUserName).EmailAddress().NotEmpty();
+            RuleFor(x => x.ProductId).NotEmpty();
+        }   
+    }
+}
