19d33cd90cef1ddd7ec4549994a48a2f7ab9dc43
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:12:55 +0300

    Order mapping added with automapper

---
+++ EAuction.Order.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EAuction.Order.Application.Commands.OrderCreate;
+using EAuction.Order.Application.Responses;
+
+namespace EAuction.Order.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Domain.Entities.Order, OrderCreateCommand>().ReverseMap();
+            CreateMap<Domain.Entities.Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
