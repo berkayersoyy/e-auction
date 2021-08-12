3a77f5459c5a9eb983adfffff843403551143594
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:12:45 +0300

    Dependency injection added for application

---
+++ EAuction.Order.Application/IOC/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using AutoMapper;
+using AutoMapper.Configuration;
+using EAuction.Order.Application.Mapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EAuction.Order.Application.IOC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            #region Configure Mapper
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+
+            #endregion
+
+            return services;
+        }
+    }
+}
