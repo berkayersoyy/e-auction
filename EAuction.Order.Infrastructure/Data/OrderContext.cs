aae6ce416663320d16aed7a69639f437e1f6700f
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:11:33 +0300

    Order context added.

---
+++ EAuction.Order.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EAuction.Order.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Domain.Entities.Order> Orders { get; set; }
+    }
+}
---
+++ EAuction.Order.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAuction.Order.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+            }
+        }
+
+        private static IEnumerable<Domain.Entities.Order> GetPreconfiguredOrders()
+        {
+            return new List<Domain.Entities.Order>()
+            {
+                new Domain.Entities.Order()
+                {
+                    AuctionId = Guid.NewGuid().ToString(),
+                    ProductId = Guid.NewGuid().ToString(),
+                    SellerUserName = "test@test.com",
+                    UnitPrice = 10,
+                    TotalPrice = 1000,
+                    CreatedAt = DateTime.UtcNow
+                },
+                new Domain.Entities.Order()
+                {
+                    AuctionId = Guid.NewGuid().ToString(),
+                    ProductId = Guid.NewGuid().ToString(),
+                    SellerUserName = "test1@test.com",
+                    UnitPrice = 10,
+                    TotalPrice = 1000,
+                    CreatedAt = DateTime.UtcNow
+                },
+                new Domain.Entities.Order()
+                {
+                    AuctionId = Guid.NewGuid().ToString(),
+                    ProductId = Guid.NewGuid().ToString(),
+                    SellerUserName = "test2@test.com",
+                    UnitPrice = 10,
+                    TotalPrice = 1000,
+                    CreatedAt = DateTime.UtcNow
+                }
+            };
+        }
+    }
+}
