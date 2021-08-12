a09328cf89c650f9cd911e5b0a015327369e7ffd
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:13:33 +0300

    Order handlers added.

---
+++ EAuction.Order.Application/Handlers/GetOrdersByUserNameHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using EAuction.Order.Application.Queries;
+using EAuction.Order.Application.Responses;
+using EAuction.Order.Domain.Repositories;
+using MediatR;
+
+namespace EAuction.Order.Application.Handlers
+{
+    public class GetOrdersByUserNameHandler:IRequestHandler<GetOrdersBySellerUserNameQuery,IEnumerable<OrderResponse>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        public GetOrdersByUserNameHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<OrderResponse>> Handle(GetOrdersBySellerUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersBySellerUserName(request.UserName);
+
+            var response = _mapper.Map<IEnumerable<OrderResponse>>(orderList);
+
+            return response;
+        }
+    }
+}
---
+++ EAuction.Order.Application/Handlers/OrderCreateHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using EAuction.Order.Application.Commands.OrderCreate;
+using EAuction.Order.Application.Responses;
+using EAuction.Order.Domain.Repositories;
+using MediatR;
+
+namespace EAuction.Order.Application.Handlers
+{
+    public class OrderCreateHandler : IRequestHandler<OrderCreateCommand,OrderResponse>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public OrderCreateHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderResponse> Handle(OrderCreateCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Domain.Entities.Order>(request);
+            if (orderEntity==null)
+            {
+                throw new ApplicationException("Entity could not be mapped!");
+            }
+
+            var order = await _orderRepository.AddAsync(orderEntity);
+
+            var orderResponse = _mapper.Map<OrderResponse>(order);
+
+            return orderResponse;
+        }
+    }
+}
