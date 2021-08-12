e98925fe7e4c356d5c4cacc7dc04a682d5906ec9
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:12:03 +0300

    Generic repository added for CRUD operations

---
+++ EAuction.Order.Infrastructure/Repositories/Base/Repository.cs
@@ -0,0 +1,69 @@
+﻿using EAuction.Order.Domain.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using EAuction.Order.Domain.Entities.Base;
+using EAuction.Order.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EAuction.Order.Infrastructure.Repositories.Base
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        protected readonly OrderContext _context;
+
+        public Repository(OrderContext context)
+        {
+            _context = context;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeStrings = null, bool disableTracking = true)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(includeStrings)) query = query.Include(includeStrings);
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
