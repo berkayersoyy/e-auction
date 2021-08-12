cf3f6c67d0a7e305e6400b2a88d2b0a63bc44b11
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:12:11 +0300

    Order repository added.

---
+++ EAuction.Order.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EAuction.Order.Domain.Repositories;
+using EAuction.Order.Infrastructure.Data;
+using EAuction.Order.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace EAuction.Order.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Domain.Entities.Order>,IOrderRepository
+    {
+
+        public OrderRepository(OrderContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Order>> GetOrdersBySellerUserName(string userName)
+        {
+            var orderList = await _context.Orders.Where(o => o.SellerUserName == userName).ToListAsync();
+            return orderList;
+        }
+    }
+}
