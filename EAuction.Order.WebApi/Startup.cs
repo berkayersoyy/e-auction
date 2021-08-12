a20a680e9e7025b10441c8aa8118193f4df1cc75
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:14:13 +0300

    Order web api added

---
+++ EAuction.Order.WebApi/Startup.cs
@@ -0,0 +1,69 @@
+using EAuction.Order.Application.IOC;
+using EAuction.Order.Infrastructure.IOC;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace EAuction.Order.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            #region Swagger Dependencies
+            services.AddSwaggerGen(c =>
+               {
+                   c.SwaggerDoc("v1", new OpenApiInfo { Title = "EAuction.Order.WebApi", Version = "v1" });
+               });
+            #endregion
+
+            #region Infrastructure Dependencies
+
+            services.AddInfrastructure(Configuration);
+
+            #endregion
+
+            #region Application Dependencies
+
+            services.AddApplication();
+
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EAuction.Order.WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
