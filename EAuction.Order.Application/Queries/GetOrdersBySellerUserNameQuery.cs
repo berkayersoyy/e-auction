2a9dd08c0e234346cba2b8e8ae8dd339f57d5174
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:13:13 +0300

    GetOrdersByUserNameQuery added.

---
+++ EAuction.Order.Application/Queries/GetOrdersBySellerUserNameQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using EAuction.Order.Application.Responses;
+using MediatR;
+
+namespace EAuction.Order.Application.Queries
+{
+    public class GetOrdersBySellerUserNameQuery : IRequest<IEnumerable<OrderResponse>>
+    {
+        public GetOrdersBySellerUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+    }
+}
