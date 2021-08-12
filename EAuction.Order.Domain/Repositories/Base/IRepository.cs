8f7efaae842ddd43eaa10b348e395c664bd2024c
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-12 14:10:59 +0300

    Base repository created.

---
+++ EAuction.Order.Domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using EAuction.Order.Domain.Entities.Base;
+
+namespace EAuction.Order.Domain.Repositories.Base
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate=null,Func<IQueryable<T>,IOrderedQueryable<T>> orderBy=null,string includeStrings = null,bool disableTracking =true);
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
