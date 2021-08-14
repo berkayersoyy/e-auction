b969d739713e093d001c182f48c05c50692f7cd2
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-14 17:59:01 +0300

    Order mapping

---
+++ EAuction.Order.WebApi/Mapping/OrderMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EAuction.Order.Application.Commands.OrderCreate;
+using EventBusRabbitMQ.Events;
+
+namespace EAuction.Order.WebApi.Mapping
+{
+    public class OrderMapping:Profile
+    {
+        public OrderMapping()
+        {
+            CreateMap<OrderCreateEvent, OrderCreateCommand>().ReverseMap();
+        }
+    }
+}
