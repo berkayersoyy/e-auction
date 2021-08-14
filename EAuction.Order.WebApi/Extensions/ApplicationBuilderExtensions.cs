086b66c99698749710e01642cda4a1b12d6ab393
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-14 17:58:54 +0300

    RabbitMQ extension added.

---
+++ EAuction.Order.WebApi/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using EAuction.Order.WebApi.Consumers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EAuction.Order.WebApi.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static EventBusOrderCreateConsumer Listener { get; set; }
+
+        public static IApplicationBuilder UseRabbitListener(this IApplicationBuilder app)
+        {
+            Listener = app.ApplicationServices.GetService<EventBusOrderCreateConsumer>();
+            var life = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            life.ApplicationStarted.Register(OnStarted);
+            life.ApplicationStopped.Register(OnStopping);
+
+            return app;
+        }
+
+        private static void OnStopping()
+        {
+            Listener.Disconnect();
+        }
+
+        private static void OnStarted()
+        {
+            Listener.Consume();
+        }
+    }
+}
