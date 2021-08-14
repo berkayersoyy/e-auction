cfe28e8855251bbfbd684e5f08d149da1ae6285d
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-14 17:58:24 +0300

    Dependency injection for infrastructure.

--- EAuction.Order.Infrastructure/IOC/DependencyInjection.cs
+++ EAuction.Order.Infrastructure/IOC/DependencyInjection.cs
@@ -14,10 +14,15 @@ namespace EAuction.Order.Infrastructure.IOC
         public static IServiceCollection AddInfrastructure(this IServiceCollection services,
             IConfiguration configuration)
         {
-            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
-                ServiceLifetime.Singleton, ServiceLifetime.Singleton);
+            //services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
+            //    ServiceLifetime.Singleton, ServiceLifetime.Singleton);
 
-            services.AddTransient(typeof(IRepository<>),typeof(Repository<>));
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("OrderConnection"),
+                    b => b.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)), ServiceLifetime.Singleton);
+
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             services.AddTransient<IOrderRepository, OrderRepository>();
 
 
