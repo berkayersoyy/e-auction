f8850a5edcfd36632cfa3dccd2f09fd80c2ae896
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-20 16:38:25 +0300

    ViewModels added.

---
+++ EAuction.UI/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EAuction.UI.ViewModel
+{
+    public class LoginViewModel
+    {
+        [EmailAddress]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [MinLength(4,ErrorMessage = "Password min 4 must be character")]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
