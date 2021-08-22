089e3245cbe6aa0d3396b0e47ebf1141c4b8aa68
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-22 15:58:00 +0300

    User repository adedd.

---
+++ EAuction.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using EAuction.Core.Entities;
+using EAuction.Core.Repositories.Abstractions;
+
+namespace EAuction.Core.Repositories
+{
+    public interface IUserRepository : IRepository<AppUser>
+    {
+        
+    }
+}
---
+++ EAuction.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using EAuction.Core.Entities;
+using EAuction.Core.Repositories;
+using EAuction.Infrastructure.Data;
+using EAuction.Infrastructure.Repositories.Base;
+
+namespace EAuction.Infrastructure.Repositories
+{
+    public class UserRepository:Repository<AppUser>,IUserRepository
+    {
+        private readonly WebAppContext _context;
+        public UserRepository(WebAppContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
