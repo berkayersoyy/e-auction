0b188baf6341dbec9bbf6c5438c02ebbf498070d
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-22 13:43:02 +0300

    Small updates.

--- EAuction.UI/Controllers/AuctionController.cs
+++ EAuction.UI/Controllers/AuctionController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using EAuction.UI.ViewModel;
 
 namespace EAuction.UI.Controllers
 {
-    [Authorize]
+
     public class AuctionController : Controller
     {
         public IActionResult Index()
+        {
+            List<AuctionViewModel> model = new List<AuctionViewModel>();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(AuctionViewModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Detail()
         {
             return View();
         }
