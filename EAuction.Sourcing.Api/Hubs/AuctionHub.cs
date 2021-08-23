e0961a3a3727356465bfda987c2ccfe3898cd693
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-23 17:57:38 +0300

    SignalR added and created auctionhub with that.

---
+++ EAuction.Sourcing.Api/Hubs/AuctionHub.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EAuction.Sourcing.Api.Hubs
+{
+    public class AuctionHub : Hub
+    {
+        public async Task AddToGroupAsync(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task SendBidAsync(string groupName,string user,string bid)
+        {
+            await Clients.Groups(groupName).SendAsync("Bids", user, bid);
+        }
+    }
+}
