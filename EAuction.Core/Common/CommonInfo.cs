3a34b588d828f8bcf59ab9133f7b05e529fa97ad
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-23 17:55:08 +0300

    Common classes created for store common data.

---
+++ EAuction.Core/Common/CommonInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace EAuction.Core.Common
+{
+    public static class CommonInfo
+    {
+        public static string LocalProductBaseAdress = "http://localhost:36886";
+        public static string LocalAuctionBaseAdress = "http://localhost:51119";
+    }
+}
---
+++ EAuction.Core/Common/ResultConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace EAuction.Core.Common
+{
+    public static class ResultConstant
+    {
+        public const string RecordFound = "Record Found";
+        public const string RecordNotFound = "Record Not Found";
+        public const string RecordCreateSuccessfully = "Record Create Successfully";
+        public const string RecordCreateNotSuccessfully = "Record Create Not Successfully";
+    }
+}
